8996be761de29bb36ceb90bed1588a68ac3f721a
Thiago Vasconcelos Teixeira <thiagovt.dev@gmail.com>
2023-02-07 11:52:26 -0300

    update Banner

--- assets/js/banner.js
+++ assets/js/banner.js
@@ -1,57 +1,45 @@
-    let balls = document.querySelector('.balls')
-    let quant = document.querySelectorAll('.slides .image')
-    let atual = 0
-    let imagem = document.getElementById('atual')
-    let next= document.getElementById('next')
-    let back = document.getElementById('back')
-    let autoScroll = true
-    
-    for (let i=0; i<quant.length; i++) {
-        let div = document.createElement('div')
-        div.id=i
-        balls.appendChild(div)
-    }
-    
-    document.getElementById('0').classList.add('imgAtual')
-    
-    let position = document.querySelectorAll('.balls div')
-    
-    for(let i=0; i<position.length; i++) {
-        position[i].addEventListener('click', ()=>{
-            atual = position[i].id
-            autoScroll=false
-            slide()
-        })
-    }
-    //()=> é uma Arrow Function usado para descrever uma função sem parametros
-    
-    back.addEventListener('click', ()=>{
-        atual--
-        autoScroll=false
-        slide()
-    })
-    next.addEventListener('click', ()=>{
-        atual++
-        autoScroll=false
-        slide()
-    })
-    
-    function slide(){
-        if(atual >= quant.length){
-            atual = 0
-        }
-        else if(atual < 0){
-            atual = quant.length-1
-        }
-        document.querySelector('.imgAtual').classList.remove('imgAtual')
-        imagem.style.marginLeft = -100*atual +'rem'
-        document.getElementById(atual).classList.add('imgAtual')
-    }
-    setInterval(()=>{
-        if(autoScroll){
-            atual ++
-            slide()
-        }else{
-            autoScroll = true
-        }
-    }, 5000)
+let slides = document.querySelector('.slides');
+let indicator = document.querySelectorAll('.indicator');
+let slideIndex = 0;
+
+setInterval(function() {
+  slideIndex = (slideIndex + 1) % 4;
+  slides.style.transform = 'translateX(-' + (25 * slideIndex) + '%)';
+  
+  for (let i = 0; i < indicator.length; i++) {
+    indicator[i].classList.remove('active');
+  }
+  
+  indicator[slideIndex].classList.add('active');
+  updateIndicator()
+}, 5000);
+
+function updateIndicator() {
+let indicator = document.querySelectorAll('.indicator');
+for (let i = 0; i < indicator.length; i++) {
+indicator[i].style.color = i === slideIndex ? 'aqua' : 'lightgray';
+}
+}
+updateIndicator()
+
+document.querySelector('#next').addEventListener('click', function() {
+  slideIndex = (slideIndex + 1) % 4;
+  slides.style.transform = 'translateX(-' + (25 * slideIndex) + '%)';
+  
+  for (let i = 0; i < indicator.length; i++) {
+    indicator[i].classList.remove('active');
+  }
+  
+  indicator[slideIndex].classList.add('active');
+});
+
+document.querySelector('#back').addEventListener('click', function() {
+  slideIndex = (slideIndex - 1 + 4) % 4;
+  slides.style.transform = 'translateX(-' + (25 * slideIndex) + '%)';
+  
+  for (let i = 0; i < indicator.length; i++) {
+    indicator[i].classList.remove('active');
+  }
+  
+  indicator[slideIndex].classList.add('active');
+});  
